4de7b40e133dd67d08f6eaa1e61b3d436ac92d52
ExpoDev <expogamer5000@gmail.com>
2020-05-23 18:02:03 -0700

    All Old sirenhead project files

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject spotlight;
+
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            spotlight.SetActive(true);
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            spotlight.SetActive(false);
+        }
+    }
+}
