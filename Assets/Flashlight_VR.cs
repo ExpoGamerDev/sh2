09be8cc79439ba2162025f704d978dfc104b09b3
KingsleyDockerill <kingsmo2008@outlook.com>
2020-06-06 17:59:49 -0400

    Flashlight + Randomly Moves

---
+++ Assets/Flashlight_VR.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight_VR : MonoBehaviour
+{
+    public GameObject flashLight;
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            flashLight.SetActive(true);
+        }
+        if(OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            flashLight.SetActive(false);
+        }
+    }
+}
